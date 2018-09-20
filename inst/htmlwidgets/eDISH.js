717e35502078f12fef86bbaa248dc435870e9865
jwildfire <jwildfire@gmail.com>
2018-09-20 13:05:39 -0700

    tweak js to ES5. #4

--- inst/htmlwidgets/eDISH.js
+++ inst/htmlwidgets/eDISH.js
@@ -14,7 +14,6 @@ HTMLWidgets.widget({
         el.innerHTML = "<div class='edish'></div>";
 
         var settings = {
-            "max_width": 600,
             "value_col": "AVAL",
             "measure_col": "PARAM",
             "visitn_col": "VISITNUM",
@@ -23,24 +22,6 @@ HTMLWidgets.widget({
             "normal_col_high": "A1HI",
             "id_col": "USUBJID",
             "group_cols": ["TRTA","RACE","AGEGR1"],
-            "filters": [
-                {
-                    "value_col": "TRTA",
-                    "label": "Treatment"
-                },
-                {
-                    "value_col": "SEX",
-                    "label": "Sex"
-                },
-                {
-                    "value_col": "RACE",
-                    "label": "Race"
-                },
-                {
-                    "value_col": "AGEGR1",
-                    "label": "Age group"
-                }
-            ],
             "measure_values":{
               "ALT":"Alanine Aminotransferase (U/L)",
               "AST":"Aspartate Aminotransferase (U/L)",
