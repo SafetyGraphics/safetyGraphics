9bbf6ea485a15b9657b0dc02a5db467b0b39bb1f
jwildfire <jwildfire@gmail.com>
2018-10-16 13:53:24 -0700

    add generateSettings() function

---
+++ tests/testthat/test_generateSettings.R
@@ -0,0 +1,45 @@
+context("Tests for the generateSettings() function")
+library(ReDish)
+setting_names<-c("id_col", "value_col", "measure_col", "normal_col_low", "normal_col_high", "studyday_col", "visit_col", "visitn_col", "baseline_visitn", "filters", "group_cols", "measure_values", "x_options", "y_options", "visit_window", "r_ratio_filter", "r_ratio_cut", "showTitle", "warningText")
+
+test_that("a list with the expected properties and structure is returned for all standards",{
+  
+  expect_is(generateSettings(standard="None"),"list")
+  expect_named(generateSettings(standard="None"),setting_names)
+  expect_named(generateSettings(standard="None")[["measure_values"]], c("ALT","AST","TB","ALP"))
+  
+  expect_is(generateSettings(standard="ADaM"),"list")
+  expect_named(generateSettings(standard="ADaM"),setting_names)
+  expect_named(generateSettings(standard="ADaM")[["measure_values"]], c("ALT","AST","TB","ALP"))
+               
+  expect_is(generateSettings(standard="SDTM"),"list")
+  expect_named(generateSettings(standard="SDTM"),setting_names)
+  expect_named(generateSettings(standard="SDTM")[["measure_values"]], c("ALT","AST","TB","ALP"))
+})
+
+test_that("a warning is thrown if chart isn't eDish",{
+  expect_warning(generateSettings(chart="aeexplorer"))
+  expect_warning(generateSettings(chart=""))
+  expect_silent(generateSettings(chart="eDish"))
+  expect_silent(generateSettings(chart="edish"))
+  expect_silent(generateSettings(chart="EdIsH"))
+})
+
+test_that("data mappings are null when setting=none, character otherwise",{
+  column_setting_names<-c("id_col", "value_col", "measure_col", "normal_col_low", "normal_col_high", "studyday_col", "visit_col", "visitn_col")
+  none_settings <- generateSettings(standard="None")
+  for(name in column_setting_names){
+    expect_null(none_settings[[name]])
+  }
+  
+  sdtm_settings <- generateSettings(standard="SDTM")
+  for(name in column_setting_names){
+    expect_is(sdtm_settings[[name]],"character")
+  }
+  
+  adam_settings <- generateSettings(standard="ADaM")
+  for(name in column_setting_names){
+    expect_is(adam_settings[[name]],"character")
+  }
+  
+})
