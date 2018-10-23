ec54698128af9f835a34d642baed07493987ba0d
jwildfire <jwildfire@gmail.com>
2018-10-23 07:34:30 -0700

    adding framework for validateSettings function

---
+++ R/validateSettings.R
@@ -0,0 +1,17 @@
+#' Compare a settings object with a specified data set
+#'
+#' This function returns a list describing the validation status of a data set for a specified data standard
+#'
+#' @param data A data frame to check against the settings object
+#' @param settings The settings list to compare with the data frame. 
+#' @return A list containing the appropriate settings for the selected chart
+
+validateSettings <- function(data, settings){
+  settingStatus<-list()
+  
+  # Check that all columns in the setting object are found in the data frame
+  
+  # Check that field level data specified in the setting object is found in the data frame
+  
+  return (settingStatus)
+}
---
+++ tests/testthat/test_validateSettings.R
@@ -0,0 +1,19 @@
+context("Tests for the validateSetting() function")
+library(ReDish)
+
+# a boolean variable called `status` is returned
+
+# a list `variableStatus` containing a status report for each data mapping setting is returned
+
+# numeric variables called `settingVariablesFound` and `settingVariablesMissing` are returned
+
+# a character variable called `validationSummary` is returned
+
+# data frame where all columns and fields are found is marked valid
+
+# data frame with a missing column is marked invalid
+
+# data frame with a missing field is marked invalid
+
+# passing an invalid data object throws an error
+
