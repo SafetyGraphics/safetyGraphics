fbfc356dd1aee7c13e70d456a142b48767c02dbd
bzkrouse <rebecca_zabel@rhoworld.com>
2018-11-01 15:30:10 -0400

    temporarily load deps/functions

--- inst/eDISH_app/global.R
+++ inst/eDISH_app/global.R
@@ -1,2 +1,15 @@
+# temporarily load deps
+library(tidyverse)
+
+# temporarily source the functions
+source('../../R/checkColumnSetting.R')
+source('../../R/checkFieldSettings.R')
+source('../../R/detectStandard.R')
+source('../../R/generateSettings.R')
+source('../../R/getSettingKeys.R')
+source('../../R/getSettingValue.R')
+source('../../R/validateSettings.R')
+
 ## source modules
-source('modules/renderSettings.R')
+ source('modules/renderSettings.R')   
+
