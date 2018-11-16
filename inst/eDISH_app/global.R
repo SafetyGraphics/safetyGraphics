96bffa0405c59732b9d479074028bd88d16c0f4e
bzkrouse <rebecca_zabel@rhoworld.com>
2018-11-16 15:36:38 -0500

    no need to source fns

--- inst/eDISH_app/global.R
+++ inst/eDISH_app/global.R
@@ -2,18 +2,18 @@
 library(tidyverse)
 library(safetyGraphics)
 
-# temporarily source the functions
-source('../../R/checkColumnSetting.R')
-source('../../R/checkFieldSettings.R')
-source('../../R/detectStandard.R')
-source('../../R/generateSettings.R')
-source('../../R/getSettingKeys.R')
-source('../../R/getSettingValue.R')
-source('../../R/validateSettings.R')
-source('../../R/compare_cols.R')
-source('../../R/getRequiredColumns.R')
-source('../../R/getRequiredSettings.R')
-source('../../R/checkSettingProvided.R')
+# # temporarily source the functions
+# source('../../R/checkColumnSetting.R')
+# source('../../R/checkFieldSettings.R')
+# source('../../R/detectStandard.R')
+# source('../../R/generateSettings.R')
+# source('../../R/getSettingKeys.R')
+# source('../../R/getSettingValue.R')
+# source('../../R/validateSettings.R')
+# source('../../R/compare_cols.R')
+# source('../../R/getRequiredColumns.R')
+# source('../../R/getRequiredSettings.R')
+# source('../../R/checkSettingProvided.R')
 
 ## source modules
  source('modules/renderSettings.R')   
