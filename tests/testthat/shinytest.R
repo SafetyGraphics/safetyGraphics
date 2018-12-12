376db9a7cd804c0b8e70b6592d8e5632e67344bc
Preston Burns <Preston_Burns@rhoworld.com>
2018-12-12 10:11:51 -0500

    add existing expecting fail

--- tests/testthat/shinytest.R
+++ tests/testthat/shinytest.R
@@ -7,14 +7,14 @@ test_that("chart is drawn and settings are valid by default", {
   # Don't run these tests on the CRAN build servers
   skip_on_cran()
   
-  appdir <- system.file(package = "shinytestPackageExample", "sampleapp")
-  expect_pass(testApp("inst/eDISH_app", "defaultPath.R"))
+  appdir <- system.file(package = "safetyGraphics", "eDISH_app")
+  expect_pass(testApp(appdir, "defaultPath.R"))
 })
 
 test_that("chart is not drawn and settings are invalid when non standard data uploaded", {
   # Don't run these tests on the CRAN build servers
   skip_on_cran()
   
-  appdir <- system.file(package = "shinytestPackageExample", "sampleapp")
-  expect_pass(testApp("inst/eDISH_app", "invalidPath.R"))
+  appdir <- system.file(package = "safetyGraphics", "eDISH_app")
+  expect_pass(testApp(appdir, "invalidPath.R"))
 })
