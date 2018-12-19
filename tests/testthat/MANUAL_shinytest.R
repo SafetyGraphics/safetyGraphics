7a37fbd0f88e39e5073c812dd84154b25d07ea0e
Preston Burns <Preston_Burns@rhoworld.com>
2018-12-19 11:35:26 -0500

    include packrat source

---
+++ tests/testthat/MANUAL_shinytest.R
@@ -0,0 +1,21 @@
+library(safetyGraphics)
+library(shinytest)
+
+context("Core App Functionality")
+# This file is for testing the applications in the inst/ directory.
+
+test_that("chart is drawn and settings are valid by default", {
+  # Don't run these tests on the CRAN build servers
+  skip_on_cran()
+  
+  appdir <- system.file(package = "safetyGraphics", "eDISH_app")
+  expect_pass(testApp(appdir, "defaultPath.R",compareImages=F))
+})
+
+# test_that("chart is not drawn and settings are invalid when non standard data uploaded", {
+#   # Don't run these tests on the CRAN build servers
+#   skip_on_cran()
+#   
+#   appdir <- system.file(package = "safetyGraphics", "eDISH_app")
+#   expect_pass(testApp(appdir, "invalidPath.R"))
+# })
