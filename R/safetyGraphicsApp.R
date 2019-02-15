c7384879fa58667f81069b0b2098ae38510b29dc
Preston Burns <s.preston.burns@gmail.com>
2019-02-15 11:46:13 -0500

    Drop rows and columns (#180)
    * increase max upload size
    
    * start working on dropping cols
    
    * complete trimData function
    
    * adding tests and starting metadata use
    
    * complete trimData and add to renderEDishChart
    
    * add file size param and change trimData to OR

--- R/safetyGraphicsApp.R
+++ R/safetyGraphicsApp.R
@@ -1,5 +1,7 @@
 #' Run the interactive safety graphics builder
 #'
+#' @param maxFileSize maximum file size in MB allowed for file upload.
+#'
 #' @importFrom shiny runApp
 #' @import shinyjs
 #' @import dplyr
@@ -10,7 +12,10 @@
 #'
 #' @export
 #'
-safetyGraphicsApp <- function() {
+safetyGraphicsApp <- function(maxFileSize = 20) {
+  #increase maximum file upload limit
+  options(shiny.maxRequestSize=(maxFileSize*1024^2))
+  
   path <- system.file("eDISH_app", package = "safetyGraphics")
   shiny::runApp(path, launch.browser = TRUE)
 }
