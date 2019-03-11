74795e44ed0eb99260c40b177f926bd0e8466e15
Preston Burns <Preston_Burns@rhoworld.com>
2019-03-11 11:29:29 -0400

    update defaults to use tibble

--- data-raw/csv_to_rda.R
+++ data-raw/csv_to_rda.R
@@ -6,18 +6,9 @@ usethis::use_data(adlbc, overwrite = TRUE)
 settingsMetadata <- read.csv("data-raw/settingsMetadata.csv")
 
 #merge defaults on to settingsMetadata
-defaults <- readRDS("data-raw/defaults.rda") #why is this not working... grrrr 
+defaults <- readRDS(file="data/defaults.rda") #why is this not working... grrrr 
 
-# from https://gist.github.com/aammd/9ae2f5cce9afd799bafb
-defaults_df <- list(default = defaults)
-class(defaults_df) <- c("tbl_df", "data.frame")
-attr(defaults_df, "row.names") <- .set_row_names(length(defaults))
-if (!is.null(names(defaults))) {
-  defaults_df$name <- names(defaults)
-}
-
-fullSettingsMetadata <- merge(settingsMetadata, defaults_df, by.x=c("text_key"),
-                              by.y=c("name"))
+fullSettingsMetadata <- merge(settingsMetadata, defaults_df, by="text_key")
 
 usethis::use_data(fullSettingsMetadata, overwrite = TRUE)
 
