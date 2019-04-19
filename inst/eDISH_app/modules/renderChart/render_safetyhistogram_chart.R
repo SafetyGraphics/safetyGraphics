faa07251f25fdfa29fa35ab7ae570e876d1b2425
bzkrouse <rebecca_zabel@rhoworld.com>
2019-04-19 06:04:45 -0400

    remove comment

--- inst/eDISH_app/modules/renderChart/render_safetyhistogram_chart.R
+++ inst/eDISH_app/modules/renderChart/render_safetyhistogram_chart.R
@@ -16,18 +16,12 @@ render_safetyhistogram_chart <- function(input, output, session, data, settings,
 
   ns <- session$ns
 
-
   # render eDISH chart if settings pass validation
   output$chart <- renderSafetyHistogram({
     req(data())
     req(settings())
 
-  #  if (valid()==TRUE){
-      #trimmed_data <- safetyGraphics:::trimData(data = data(), settings = settings())
       safetyHistogram(data = data(), settings = settings())
-    # } else{
-    #   return()
-    # }
   })
 
 }
