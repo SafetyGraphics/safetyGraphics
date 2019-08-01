ac12b7b14c5e4c5bfe5c1f17dc2a9250c09d4086
Preston Burns <Preston_Burns@rhoworld.com>
2019-08-01 14:05:41 -0400

    add package to app

--- inst/safetyGraphics_app/global.R
+++ inst/safetyGraphics_app/global.R
@@ -11,6 +11,7 @@ library(stringr)
 library(DT)
 library(haven)
 library(tidyr)
+library(shinybusy)
 
 # subset chartsMetadata if user requests it
 if (!is.null(getShinyOption("safetygraphics_charts"))){
