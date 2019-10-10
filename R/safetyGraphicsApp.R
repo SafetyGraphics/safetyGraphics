78f3ab7cabec01b89b09490000d365f690ca6285
jwildfire <jwildfire@gmail.com>
2019-10-10 09:15:34 -0300

    add support for custom script in safetyGraphicsApp

--- R/safetyGraphicsApp.R
+++ R/safetyGraphicsApp.R
@@ -2,7 +2,9 @@
 #'
 #' @param charts Character vector of charts to include 
 #' @param maxFileSize maximum file size in MB allowed for file upload
-#' @param settingsLocation folder location of user-defined settings metadata
+#' @param settingsLocation folder location of user-defined settings metadata. Files should be named settingsMetadata.rda, chartsMetadata.rda and standardsMetadata.rda and use the same structure established in the /data folder. 
+#' @param customSettings Name of R script containing settings customizations to be run before the app is initialized. This is the recommended way to add additional charts (via addChart()), settings (addSetting()) and data standards (addStandard()). default = 'settingsLocation/customSettings.R'
+
 #'
 #' @importFrom shiny runApp shinyOptions
 #' @import shinyjs
@@ -17,7 +19,7 @@
 #'
 #' @export
 #'
-safetyGraphicsApp <- function(charts = NULL, maxFileSize = NULL, settingsLocation = NULL) {
+safetyGraphicsApp <- function(charts = NULL, maxFileSize = NULL, settingsLocation = NULL, customSettings="customSettings.R") {
   
   # pass charts to include
   shiny::shinyOptions(safetygraphics_charts = charts)
@@ -30,6 +32,10 @@ safetyGraphicsApp <- function(charts = NULL, maxFileSize = NULL, settingsLocatio
     options(shiny.maxRequestSize=(maxFileSize*1024^2))  
   }
   
+  # run the custom settings file (if it exists)
+  customSettingsScript<-paste(settingsLocation, customSettings,sep="/")
+  if(file.exists(customSettingsScript)) source(customSettingsScript)
+    
   path <- system.file("safetyGraphics_app", package = "safetyGraphics")
   shiny::runApp(path, launch.browser = TRUE)
 }
