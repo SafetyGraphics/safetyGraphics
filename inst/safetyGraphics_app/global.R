3bcc6f4ffee6be7255d1144c86df0b7e271f13a9
Rebecca Krouse <rzabel@ms.rhoworld.com>
2020-02-28 15:54:44 -0500

    temporarily filter metadata to single domain

--- inst/safetyGraphics_app/global.R
+++ inst/safetyGraphics_app/global.R
@@ -13,14 +13,13 @@ library(haven)
 library(tidyr)
 library(shinybusy)
 
-
-# remove settings and charts metadata in the workspace to avoid carryover from previous instances of the app. 
+# remove settings and charts metadata in the workspace to avoid carryover from previous instances of the app.
 if(exists("settingsMetadata", inherits = FALSE)){
-  rm("settingsMetadata")  
+  rm("settingsMetadata")
 }
 
 if(exists("chartsMetadata", inherits = FALSE)){
-  rm("chartsMetadata")  
+  rm("chartsMetadata")
 }
 
 # use metadata in user settings folder if provided
@@ -33,6 +32,12 @@ if (options('sg_settingsMetadata')[[1]]){
 if (options('sg_standardsMetadata')[[1]]){
   standardsMetadata <-  options('sg_standardsMetadata_df')[[1]]
 }
+
+# temporary! until we move this part to the modules
+settingsMetadata <- subset(settingsMetadata, domain=="labs")
+chartsMetadata <- subset(chartsMetadata, domain=="labs")
+standardsMetadata <- subset(standardsMetadata, domain=="labs")
+
 
 # subset chartsMetadata if user requests it
 if (!is.null(getShinyOption("safetygraphics_charts"))){
@@ -47,25 +52,25 @@ if (!is.null(getShinyOption("safetygraphics_charts"))){
 # pre-load data into app if requested
 if(getShinyOption("sg_loadData")){
   preload_data_list <- list()
-  
+
   # names of data in environment
   dat_names <- ls(pos=1)[sapply(ls(pos=1), function(x) inherits(get(x), "data.frame"))]
   dat_names <- dat_names[!dat_names %in% c("chartsMetadata","standardsMetdata","settingsMetadata")]
-  
+
   preload_data_list$data <- lapply(dat_names, function(x) {get(x)})
   names(preload_data_list$data) <- dat_names
-  
+
   # set all to not currently selected
   preload_data_list$current <- c(1, rep(0, length(dat_names)-1))
-  
+
   # detect standard for all datasets
   preload_data_list$standard <- lapply(preload_data_list$data, function(x){ detectStandard(x) })
 
   # get display name for all datasets
   preload_data_list$display <- list()
-  
+
   for (i in 1:length(dat_names)){
-    
+
     temp_standard <- preload_data_list$standard[[i]]$standard
     standard_label <- ifelse(temp_standard=="adam","AdAM",ifelse(temp_standard=="sdtm","SDTM",temp_standard))
     if(temp_standard == "none") {
@@ -74,12 +79,12 @@ if(getShinyOption("sg_loadData")){
       preload_data_list$display[[i]] <- HTML(paste0("<p>", names(preload_data_list$data)[i], " - <em style='color:green; font-size:12px;'>", standard_label, "</em></p>"))
       # If partial data spec match - give the fraction of variables matched
     } else {
-      
+
       valid_count <- preload_data_list$standard[[i]]$details[[temp_standard]]$valid_count
       total_count <- preload_data_list$standard[[i]]$details[[temp_standard]]$invalid_count + valid_count
-      
+
       fraction_cols  <- paste0(valid_count, "/" ,total_count)
-      
+
       preload_data_list$display[[i]] <- HTML(paste0("<p>", names(preload_data_list$data)[i], " - <em style='color:green; font-size:12px;'>", "Partial ",
                                   standard_label, " (", fraction_cols, " data settings)",  "</em></p>"))
     }
@@ -103,3 +108,7 @@ source('modules/renderReports/renderReports.R')
 
 source('modules/dataUpload/dataUploadUI.R')
 source('modules/dataUpload/dataUpload.R')
+
+
+
+
