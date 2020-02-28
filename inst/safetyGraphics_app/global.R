4c93fad28c937812f82494bb5aede30746f3ed6f
Rebecca Krouse <rzabel@ms.rhoworld.com>
2020-02-28 16:54:29 -0500

    expand preloaded data to include domains (remove user preload for now)

--- inst/safetyGraphics_app/global.R
+++ inst/safetyGraphics_app/global.R
@@ -33,11 +33,6 @@ if (!is.null(options('sg_standardsMetadata')[[1]]) && options('sg_standardsMetad
   standardsMetadata <-  options('sg_standardsMetadata_df')[[1]]
 }
 
-# temporary! until we move this part to the modules
-settingsMetadata <- subset(settingsMetadata, domain=="labs")
-chartsMetadata <- subset(chartsMetadata, domain=="labs")
-standardsMetadata <- subset(standardsMetadata, domain=="labs")
-
 
 # subset chartsMetadata if user requests it
 if (!is.null(getShinyOption("safetygraphics_charts"))){
@@ -50,51 +45,57 @@ if (!is.null(getShinyOption("safetygraphics_charts"))){
 
 # Prepare initial datasets/labels (with info about standards) to be loaded into the app 
 # pre-load data into app if requested
-if(!is.null(getShinyOption("sg_loadData")) && getShinyOption("sg_loadData")){
-  preload_data_list <- list()
-
-  # names of data in environment
-  dat_names <- ls(pos=1)[sapply(ls(pos=1), function(x) inherits(get(x), "data.frame"))]
-  dat_names <- dat_names[!dat_names %in% c("chartsMetadata","standardsMetdata","settingsMetadata")]
-
-  preload_data_list$data <- lapply(dat_names, function(x) {get(x)})
-  names(preload_data_list$data) <- dat_names
-
-  # set all to not currently selected
-  preload_data_list$current <- c(1, rep(0, length(dat_names)-1))
-
-  # detect standard for all datasets
-  preload_data_list$standard <- lapply(preload_data_list$data, function(x){ detectStandard(x) })
-
-  # get display name for all datasets
-  preload_data_list$display <- list()
-
-  for (i in 1:length(dat_names)){
-
-    temp_standard <- preload_data_list$standard[[i]]$standard
-    standard_label <- ifelse(temp_standard=="adam","AdAM",ifelse(temp_standard=="sdtm","SDTM",temp_standard))
-    if(temp_standard == "none") {
-      preload_data_list$display[[i]] <- HTML(paste0("<p>", names(preload_data_list$data)[i], " - <em style='font-size:12px;'>No Standard Detected</em></p>"))
-    } else if (preload_data_list$standard[[i]]$details[[temp_standard]]$match == "full") {
-      preload_data_list$display[[i]] <- HTML(paste0("<p>", names(preload_data_list$data)[i], " - <em style='color:green; font-size:12px;'>", standard_label, "</em></p>"))
-      # If partial data spec match - give the fraction of variables matched
-    } else {
-
-      valid_count <- preload_data_list$standard[[i]]$details[[temp_standard]]$valid_count
-      total_count <- preload_data_list$standard[[i]]$details[[temp_standard]]$invalid_count + valid_count
-
-      fraction_cols  <- paste0(valid_count, "/" ,total_count)
-
-      preload_data_list$display[[i]] <- HTML(paste0("<p>", names(preload_data_list$data)[i], " - <em style='color:green; font-size:12px;'>", "Partial ",
-                                  standard_label, " (", fraction_cols, " data settings)",  "</em></p>"))
-    }
-  }
-} else {  # otherwise use example data
-  preload_data_list <- list(data = list("Example data" = labs),
+# if(!is.null(getShinyOption("sg_loadData")) && getShinyOption("sg_loadData")){
+#   preload_data_list <- list()
+# 
+#   # names of data in environment
+#   dat_names <- ls(pos=1)[sapply(ls(pos=1), function(x) inherits(get(x), "data.frame"))]
+#   dat_names <- dat_names[!dat_names %in% c("chartsMetadata","standardsMetdata","settingsMetadata")]
+# 
+#   preload_data_list$data <- lapply(dat_names, function(x) {get(x)})
+#   names(preload_data_list$data) <- dat_names
+# 
+#   # set all to not currently selected
+#   preload_data_list$current <- c(1, rep(0, length(dat_names)-1))
+# 
+#   # detect standard for all datasets
+#   preload_data_list$standard <- lapply(preload_data_list$data, function(x){ detectStandard(x) })
+# 
+#   # get display name for all datasets
+#   preload_data_list$display <- list()
+# 
+#   for (i in 1:length(dat_names)){
+# 
+#     temp_standard <- preload_data_list$standard[[i]]$standard
+#     standard_label <- ifelse(temp_standard=="adam","AdAM",ifelse(temp_standard=="sdtm","SDTM",temp_standard))
+#     if(temp_standard == "none") {
+#       preload_data_list$display[[i]] <- HTML(paste0("<p>", names(preload_data_list$data)[i], " - <em style='font-size:12px;'>No Standard Detected</em></p>"))
+#     } else if (preload_data_list$standard[[i]]$details[[temp_standard]]$match == "full") {
+#       preload_data_list$display[[i]] <- HTML(paste0("<p>", names(preload_data_list$data)[i], " - <em style='color:green; font-size:12px;'>", standard_label, "</em></p>"))
+#       # If partial data spec match - give the fraction of variables matched
+#     } else {
+# 
+#       valid_count <- preload_data_list$standard[[i]]$details[[temp_standard]]$valid_count
+#       total_count <- preload_data_list$standard[[i]]$details[[temp_standard]]$invalid_count + valid_count
+# 
+#       fraction_cols  <- paste0(valid_count, "/" ,total_count)
+# 
+#       preload_data_list$display[[i]] <- HTML(paste0("<p>", names(preload_data_list$data)[i], " - <em style='color:green; font-size:12px;'>", "Partial ",
+#                                   standard_label, " (", fraction_cols, " data settings)",  "</em></p>"))
+#     }
+#   }
+# } else {  # otherwise use example data
+  preload_data_list <- list(labs = list(
+    data = list("Example data" = labs),
                             current = 1,
                             standard = list(list("standard" = "adam", "details" = list("adam"=list("match"="full")))),
-                            display = list(HTML("<p>Example data - <em style='color:green; font-size:12px;'>ADaM</em></p>")))
-}
+                            display = list(HTML("<p>Example data - <em style='color:green; font-size:12px;'>ADaM</em></p>"))),
+    aes = list(
+      data = list("Example data" = aes),
+      current = 1,
+      standard = list(list("standard" = "sdtm", "details" = list("sdtm"=list("match"="full")))),
+      display = list(HTML("<p>Example data - <em style='color:green; font-size:12px;'>ADaM</em></p>"))))
+# }
 
 ## source modules
 source('modules/renderSettings/renderSettingsUI.R')
