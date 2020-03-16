43660852a62750564ec40903862543100f3d6c57
jwildfire <jwildfire@gmail.com>
2020-03-16 12:25:37 -0400

    fix evaluateStandard tests

--- tests/testthat/test_evaluateStandard.R
+++ tests/testthat/test_evaluateStandard.R
@@ -1,14 +1,15 @@
 context("Tests for the evaluateStandard() function")
 library(safetyGraphics)
 
+lm <- getMetadata(domain="labs")
 test_that("basic test cases evaluate as expected",{
-  expect_equal(evaluateStandard(data=labs, standard="adam")[["match"]],"full")
-  expect_equal(evaluateStandard(data=labs, standard="sdtm")[["match"]],"partial")
-  expect_equal(evaluateStandard(data=data.frame(), standard="sdtm")[["match"]],"none")
+  expect_equal(evaluateStandard(data=labs, standard="adam", dmeta=lm)[["match"]],"full")
+  expect_equal(evaluateStandard(data=labs, standard="sdtm", dmeta=lm)[["match"]],"partial")
+  expect_equal(evaluateStandard(data=data.frame(), standard="sdtm", dmeta=lm)[["match"]],"none")
 })
 
 test_that("a list with the expected properties and structure is returned",{
-  a<- evaluateStandard(data=data.frame(),standard="adam")
+  a<- evaluateStandard(data=data.frame(),standard="adam", dmeta=lm)
 
   expect_is(a,"list")
   expect_named(a,c('standard', 'checks', 'total_count','valid_count', 'invalid_count','match_percent', 'match'))
@@ -20,12 +21,12 @@ test_that("a list with the expected properties and structure is returned",{
 })
 
 test_that("expected number of checks (in)valid",{
-  expect_equal(evaluateStandard(data=labs, standard="sdtm")[["valid_count"]],3)
-  expect_equal(evaluateStandard(data=labs, standard="sdtm")[["invalid_count"]],9)
+  expect_equal(evaluateStandard(data=labs, standard="sdtm",dmeta=lm)[["valid_count"]],3)
+  expect_equal(evaluateStandard(data=labs, standard="sdtm",dmeta=lm)[["invalid_count"]],9)
   
   labs_edit <- labs
   labs_edit$TEST <- labs_edit$PARAM
-  a<-evaluateStandard(data=labs_edit, standard="sdtm")
+  a<-evaluateStandard(data=labs_edit, standard="sdtm",dmeta=lm)
   expect_equal(a[["valid_count"]],4)
   expect_equal(a[["invalid_count"]],8)
   expect_equal(a[["total_count"]],12)
@@ -35,16 +36,17 @@ test_that("expected number of checks (in)valid",{
 
 
 test_that("field level data is ignored when useFields=false",{
-  noFields<-evaluateStandard(data=labs, standard="adam", includeFields=FALSE)
+  noFields<-evaluateStandard(data=labs, standard="adam", includeFields=FALSE, dmeta=lm)
   expect_equal(noFields[["match"]],"full")
   expect_equal(noFields[["match_percent"]],1)
   expect_equal(noFields[["valid_count"]],8)
 })
 
 test_that("invalid options throw errors",{
-  expect_error(evaluateStandard(data=list(a=1,b=2), standard="sdtm"))
-  expect_error(evaluateStandard(data="notadataframe", standard="sdtm"))
-  expect_error(evaluateStandard(data=labs, standard=123))
-  expect_error(evaluateStandard(data=labs, standard="notarealstandard"))
-  expect_error(evaluateStandard(data=labs, standard="adam", includeFields="yesPlease"))
+  expect_error(evaluateStandard(data=labs, standard="adam")) #no metadata
+  expect_error(evaluateStandard(data=list(a=1,b=2), standard="sdtm", dmeta=lm))
+  expect_error(evaluateStandard(data="notadataframe", standard="sdtm", dmeta=lm))
+  expect_error(evaluateStandard(data=labs, standard=123, dmeta=lm))
+  expect_error(evaluateStandard(data=labs, standard="notarealstandard", dmeta=lm))
+  expect_error(evaluateStandard(data=labs, standard="adam", includeFields="yesPlease", dmeta=lm))
 })
