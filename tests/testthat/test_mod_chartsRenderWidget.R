0f432ee501703cc465b04d2aa29fbc537d9ee00b
jwildfire <jwildfire@gmail.com>
2021-02-04 19:47:36 -0500

    try adding sys.sleep()

--- tests/testthat/test_mod_chartsRenderWidget.R
+++ tests/testthat/test_mod_chartsRenderWidget.R
@@ -20,6 +20,7 @@ app <- ShinyDriver$new("./module_examples/chartsRenderWidget")
 # })
 
 test_that("2nd widget renderers by default",{
+  Sys.sleep(5)
   outputs2<-app$getAllValues()[["output"]]
   expect_named(outputs2, c("ex2-widgetChart"))
   chart2<-jsonlite::fromJSON(outputs2[["ex2-widgetChart"]])
@@ -28,6 +29,7 @@ test_that("2nd widget renderers by default",{
 
 test_that("3rd widget renderers when sidebar is clicked",{
   app$setInputs(sidebar_tabs = "ex3-tab")
+  Sys.sleep(5)
   outputs3<-app$getAllValues()[["output"]]
   expect_named(outputs3, c("ex2-widgetChart","ex3-widgetChart"))
   chart3<-jsonlite::fromJSON(outputs3[["ex3-widgetChart"]])
