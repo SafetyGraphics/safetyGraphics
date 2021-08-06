93bad99119fef3aba577bd2c81d762074e54fd33
Jeremy Wildfire <jwildfire@gmail.com>
2021-08-06 09:56:48 -0400

    create add/remove charts buttons. fix #569

--- R/mod_loadCharts.R
+++ R/mod_loadCharts.R
@@ -11,27 +11,14 @@
 
 loadChartsUI <- function(id, charts=makeChartConfig()){ 
     ns <- NS(id)
-    inactive <- charts%>%keep(~.x$order < 1)%>%map(~makeChartSummary(.x,showLinks=FALSE,class="chart-sortable"))
-    active <- charts%>%keep(~.x$order >= 1)%>%map(~makeChartSummary(.x,showLinks=FALSE,class="chart-sortable"))
-
     div(
-        sortable::bucket_list(
-            header = h4("Chart Loader"),
-            group_name = ns("chartList"),
-            orientation = "horizontal",
-            add_rank_list(
-                text = "Active Charts",
-                labels = active,
-                input_id = ns("active")
-            ),
-            add_rank_list(
-                text = "Inactive Charts",
-                labels = inactive,
-                input_id = ns("inactive")
-            )
-        )
-    )
-
+        h4(
+            "Chart Loader",
+            actionButton(ns("addCharts"), "Select All", class="btn-xs"),
+            actionButton(ns("removeCharts"), "Remove All", class="btn-xs")
+        ),        
+        uiOutput(ns("chartLists"))
+    ) 
 }
 
 #' @title   loadCharts
@@ -44,6 +31,41 @@ loadChartsUI <- function(id, charts=makeChartConfig()){
 
 #' @export
 loadCharts <- function(input, output, session, charts=makeChartConfig()) {
+    ns<-session$ns
+    labels<-charts%>%map(~makeChartSummary(.x,showLinks=FALSE,class="chart-sortable"))
+    rv <- reactiveValues(
+        inactive = charts%>%keep(~.x$order < 1)%>%map(~makeChartSummary(.x,showLinks=FALSE,class="chart-sortable")),
+        active = charts%>%keep(~.x$order >= 1)%>%map(~makeChartSummary(.x,showLinks=FALSE,class="chart-sortable"))
+    )
+    output$chartLists <- renderUI({
+        div(
+            sortable::bucket_list(
+                header = NULL,
+                group_name = ns("chartList"),
+                orientation = "horizontal",
+                add_rank_list(
+                    text = "Active Charts",
+                    labels = rv$active,
+                    input_id = ns("active")
+                ),
+                add_rank_list(
+                    text = "Inactive Charts",
+                    labels = rv$inactive,
+                    input_id = ns("inactive")
+                )
+            )
+        )
+    })
+
+    observeEvent(input$addCharts,{
+        rv$active <- labels
+        rv$inactive <- NULL
+    })
+
+    observeEvent(input$removeCharts,{
+        rv$active <- NULL
+        rv$inactive <- labels
+    })
     chartsR<-reactive({
         charts %>% 
             purrr::keep(~.x$name %in% input$active) %>%
